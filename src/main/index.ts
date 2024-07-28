e766d4094029f3897eeeb5dc4acd388378adcf08
holodnyak <holodnyak@stilsoft.ru>
2024-07-28 03:59:07 +0300

    Рефакторинг

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,54 +1,9 @@
-import { app, shell, BrowserWindow } from 'electron'
-import { join } from 'path'
+import { app, BrowserWindow } from 'electron'
 import { electronApp, optimizer } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
 import { setupJsonServer } from './setupJsonServer'
 import 'dotenv/config'
 import { registerHotkeys } from './utils/registerHotkeys'
-
-const isDev = process.env.IS_DEV === 'true'
-function createWindow(): void {
-  const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
-    show: false,
-    autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
-    webPreferences: {
-      sandbox: false,
-      devTools: isDev
-    }
-  })
-
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
-
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
-
-  if (process.env.STILJIRA_URL) {
-    mainWindow.loadURL(process.env.STILJIRA_URL)
-    console.log('loaded from url', process.env.STILJIRA_URL)
-
-    mainWindow.webContents.on('console-message', (_event, _lvl, message) => {
-      if (message.includes('server connection lost')) {
-        console.log('console-message:', message)
-        mainWindow.loadFile(join(__dirname, '../renderer/dist/index.html'))
-      }
-    })
-  } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/dist/index.html'))
-    console.log('loaded from static')
-  }
-
-  mainWindow.webContents.on('did-fail-load', () => {
-    console.log('WINDOW did-fail-load ERROR OCCURRED')
-    mainWindow.loadFile(join(__dirname, '../renderer/dist/index.html'))
-  })
-}
+import { createWindow } from './utils/createWindow'
 
 app.whenReady().then(async () => {
   registerHotkeys()
---
+++ src/main/utils/createWindow.ts
@@ -0,0 +1,48 @@
+import { BrowserWindow, shell } from 'electron'
+import { join } from 'path'
+import icon from '../../../resources/icon.png?asset'
+
+const isDev = process.env.IS_DEV === 'true'
+
+export function createWindow(): void {
+  const mainWindow = new BrowserWindow({
+    width: 900,
+    height: 670,
+    show: false,
+    autoHideMenuBar: true,
+    ...(process.platform === 'linux' ? { icon } : {}),
+    webPreferences: {
+      sandbox: false,
+      devTools: isDev
+    }
+  })
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url)
+    return { action: 'deny' }
+  })
+
+  if (process.env.STILJIRA_URL) {
+    mainWindow.loadURL(process.env.STILJIRA_URL)
+    console.log('loaded from url', process.env.STILJIRA_URL)
+
+    mainWindow.webContents.on('console-message', (_event, _lvl, message) => {
+      if (message.includes('server connection lost')) {
+        console.log('console-message:', message)
+        mainWindow.loadFile(join(__dirname, '../renderer/dist/index.html'))
+      }
+    })
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/dist/index.html'))
+    console.log('loaded from static')
+  }
+
+  mainWindow.webContents.on('did-fail-load', () => {
+    console.log('WINDOW did-fail-load ERROR OCCURRED')
+    mainWindow.loadFile(join(__dirname, '../renderer/dist/index.html'))
+  })
+}
