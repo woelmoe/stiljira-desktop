557e9a66f51e8a60e0a6edda499a0a240b49f83e
holodnyak <holodnyak@stilsoft.ru>
2024-08-04 21:10:20 +0300

    Рефакторинг

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,5 @@
 import { app, BrowserWindow } from 'electron'
-import { electronApp, optimizer } from '@electron-toolkit/utils'
+import { optimizer } from '@electron-toolkit/utils'
 import { setupJsonServer } from './setupJsonServer'
 import 'dotenv/config'
 import { registerHotkeys } from './utils/registerHotkeys'
@@ -8,7 +8,6 @@ import { createWindow } from './utils/createWindow'
 app.whenReady().then(async () => {
   registerHotkeys()
   await setupJsonServer()
-  electronApp.setAppUserModelId('com.electron')
 
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
